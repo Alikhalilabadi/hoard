a909398f9a3f984cc0b0f25d995c6bb0b4699202
sauravb <s.bhatta2383@gmail.com>
2024-11-14 19:39:41 -0800

    Initial setup of client side Hoard app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+    return ( <div>hNavbar</div> );
+}
+ 
+export default Navbar;
---
+++ src/pages/marketplace.jsx
@@ -0,0 +1,5 @@
+const MarketPlace = () => {
+    return ( <div>Market Place</div> );
+}
+ 
+export default MarketPlace;
